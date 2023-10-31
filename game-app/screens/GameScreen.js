25a7ecd4b8addf9ff2108a062fb8fc5f96659a4b
Esteban de la Torre <edelatorre94@gmail.com>
2023-10-30 21:29:37 -0700

    7.78 Add guess rounds

--- game-app/screens/GameScreen.js
+++ game-app/screens/GameScreen.js
@@ -28,7 +28,7 @@ function GameScreen({ userNumber, onGameOver }) {
 
   useEffect(() => {
     if (currentGuess === userNumber) {
-      onGameOver();
+      onGameOver(guessRounds.length);
     }
   }, [currentGuess, userNumber, onGameOver]);
 
@@ -89,7 +89,7 @@ function GameScreen({ userNumber, onGameOver }) {
           </View>
         </View>
       </Card>
-      <View>
+      <View style={styles.listContainer}>
         <FlatList
           data={guessRounds}
           renderItem={(itemData) => (
@@ -123,5 +123,9 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flex: 1,
+  },
+  listContainer: {
+    flex: 1,
+    padding: 16,
   },
 });
