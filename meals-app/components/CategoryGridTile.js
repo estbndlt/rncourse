cbc241d8a76ef6b36b98b6e1f766d7272abe74df
Esteban de la Torre <edelatorre94@gmail.com>
2023-11-06 21:19:04 -0800

    6.93 Add styling to grid tiles

--- meals-app/components/CategoryGridTile.js
+++ meals-app/components/CategoryGridTile.js
@@ -1,14 +1,43 @@
-import { Pressable, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 const CategoryGridTile = ({ title, color }) => {
   return (
-    <View>
-      <Pressable></Pressable>
-      <View>
-        <Text>{title}</Text>
-      </View>
+    <View style={styles.gridItem}>
+      <Pressable style={styles.button}>
+        <View style={styles.innerContainer}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+      </Pressable>
     </View>
   );
 };
 
 export default CategoryGridTile;
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: 'black',
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    backgroundColor: 'white',
+  },
+  button: {
+    flex: 1,
+  },
+  innerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+});
