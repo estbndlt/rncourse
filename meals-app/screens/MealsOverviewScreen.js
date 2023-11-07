fc725d0cb28ece9ff1ea4d0c67fb6bce75e35980
Esteban de la Torre <edelatorre94@gmail.com>
2023-11-07 08:28:38 -0800

    6.99 Add meal item component

--- meals-app/screens/MealsOverviewScreen.js
+++ meals-app/screens/MealsOverviewScreen.js
@@ -1,11 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 const MealsOverviewScreen = ({ route }) => {
   const catId = route.params.categoryId;
+  const displayMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  const renderMealItem = (itemData) => {
+    return <MealItem title={itemData.item.title} />;
+  };
 
   return (
     <View style={styles.container}>
       <Text>Meals Overview Screen - {catId}</Text>
+      <FlatList
+        data={displayMeals}
+        keyExtractor={(item) => {
+          item.id;
+        }}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 };
