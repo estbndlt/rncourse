e23c574c9032fa3ae162deb099a7da082df6f966
Esteban de la Torre <edelatorre94@gmail.com>
2023-11-08 08:09:50 -0800

    6.109 Add styling to WelcomeScreen drawer

--- other-navigation-app-section-6/App.js
+++ other-navigation-app-section-6/App.js
@@ -10,7 +10,18 @@ export default function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator>
-        <Drawer.Screen name="Welcome" component={WelcomeScreen} />
+        <Drawer.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{
+            headerStyle: { backgroundColor: '#3c0a6b' },
+            headerTintColor: 'white',
+            drawerLabel: 'Welcome Screen',
+            drawerActiveBackgroundColor: '#f0e1ff',
+            drawerActiveTintColor: '#3c0a6b',
+            drawerStyle: { backgroundColor: '#ccc' },
+          }}
+        />
         <Drawer.Screen name="User" component={UserScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
