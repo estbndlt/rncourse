742a838881475f97d03e8c1141410ef460fd9a5a
Esteban de la Torre <edelatorre94@gmail.com>
2023-11-08 08:11:59 -0800

    6.109 Move drawer styling to all screens

--- other-navigation-app-section-6/App.js
+++ other-navigation-app-section-6/App.js
@@ -9,17 +9,20 @@ const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
+      <Drawer.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: '#3c0a6b' },
+          headerTintColor: 'white',
+          drawerActiveBackgroundColor: '#f0e1ff',
+          drawerActiveTintColor: '#3c0a6b',
+          // drawerStyle: { backgroundColor: '#ccc' },
+        }}
+      >
         <Drawer.Screen
           name="Welcome"
           component={WelcomeScreen}
           options={{
-            headerStyle: { backgroundColor: '#3c0a6b' },
-            headerTintColor: 'white',
             drawerLabel: 'Welcome Screen',
-            drawerActiveBackgroundColor: '#f0e1ff',
-            drawerActiveTintColor: '#3c0a6b',
-            drawerStyle: { backgroundColor: '#ccc' },
           }}
         />
         <Drawer.Screen name="User" component={UserScreen} />
