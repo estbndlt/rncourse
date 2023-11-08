5c0156ee204dbf608e3fb69e4ebdd9c5fa5db34e
Esteban de la Torre <edelatorre94@gmail.com>
2023-11-07 21:51:03 -0800

    6.104 Create MealDetails.js to encapsulate reused functionality

--- meals-app/screens/MealDetailScreen.js
+++ meals-app/screens/MealDetailScreen.js
@@ -1,6 +1,7 @@
 import { useLayoutEffect } from 'react';
-import { Image, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import { MEALS } from '../data/dummy-data';
+import MealDetails from '../components/MealDetails';
 
 const MealDetailScreen = ({ route, navigation }) => {
   const mealId = route.params.mealId;
@@ -14,9 +15,13 @@ const MealDetailScreen = ({ route, navigation }) => {
 
   return (
     <View>
-      <Image />
-      <Text>Meal Details Screen - {mealId}</Text>
-      <View></View>
+      <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+      <Text>{selectedMeal.title}</Text>
+      <MealDetails
+        duration={selectedMeal.duration}
+        complexity={selectedMeal.complexity}
+        affordability={selectedMeal.affordability}
+      />
       <Text>Ingredients</Text>
       <Text>Steps</Text>
     </View>
@@ -24,3 +29,10 @@ const MealDetailScreen = ({ route, navigation }) => {
 };
 
 export default MealDetailScreen;
+
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 200,
+  },
+});
