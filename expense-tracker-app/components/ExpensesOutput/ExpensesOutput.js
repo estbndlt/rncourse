0fcc77bd63ecb9abacb945f9c934b6dc4fc751fe
Esteban de la Torre <edelatorre94@gmail.com>
2023-11-12 22:01:12 -0800

    8.129 Move ExpensesOutput contents to components

--- expense-tracker-app/components/ExpensesOutput/ExpensesOutput.js
+++ expense-tracker-app/components/ExpensesOutput/ExpensesOutput.js
@@ -3,10 +3,10 @@ import { View } from 'react-native';
 import ExpensesSummary from './ExpensesSummary';
 import ExpensesList from './ExpensesList';
 
-const ExpensesOutput = ({ expenses }) => {
+const ExpensesOutput = ({ expenses, periodName }) => {
   return (
     <View>
-      <ExpensesSummary />
+      <ExpensesSummary expenses={expenses} periodName={periodName} />
       <ExpensesList />
     </View>
   );
