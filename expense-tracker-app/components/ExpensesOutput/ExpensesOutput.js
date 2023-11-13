47306f0f43f7078afd66fcb552196509735dfd61
Esteban de la Torre <edelatorre94@gmail.com>
2023-11-12 22:08:09 -0800

    8.131 Add dummy data and ExpensesOutput to screens

--- expense-tracker-app/components/ExpensesOutput/ExpensesOutput.js
+++ expense-tracker-app/components/ExpensesOutput/ExpensesOutput.js
@@ -3,11 +3,44 @@ import { View } from 'react-native';
 import ExpensesSummary from './ExpensesSummary';
 import ExpensesList from './ExpensesList';
 
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    description: 'A pair of shoes',
+    amount: 59.99,
+    date: new Date('2021-12-19'),
+  },
+  {
+    id: 'e2',
+    description: 'Trousers',
+    amount: 89.29,
+    date: new Date('2022-01-05'),
+  },
+  {
+    id: 'e3',
+    description: 'Bananas',
+    amount: 5.99,
+    date: new Date('2021-12-01'),
+  },
+  {
+    id: 'e4',
+    description: 'Book',
+    amount: 14.99,
+    date: new Date('2022-02-19'),
+  },
+  {
+    id: 'e5',
+    description: 'Cup',
+    amount: 18.59,
+    date: new Date('2022-02-18'),
+  },
+];
+
 const ExpensesOutput = ({ expenses, periodName }) => {
   return (
     <View>
-      <ExpensesSummary expenses={expenses} periodName={periodName} />
-      <ExpensesList />
+      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={periodName} />
+      <ExpensesList expenses={DUMMY_EXPENSES} />
     </View>
   );
 };
--- expense-tracker-app/screens/RecentExpenses.js
+++ expense-tracker-app/screens/RecentExpenses.js
@@ -1,7 +1,7 @@
-import { Text } from 'react-native';
+import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
 
 const RecentExpense = () => {
-  return <Text>RecentExpense Screen</Text>;
+  return <ExpensesOutput expensesPeriod={'Last 7 Days'} />;
 };
 
 export default RecentExpense;
