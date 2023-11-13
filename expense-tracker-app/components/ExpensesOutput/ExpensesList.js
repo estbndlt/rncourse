6cab19fe8515776cdc5615d86388128d12c1cec2
Esteban de la Torre <edelatorre94@gmail.com>
2023-11-12 21:51:16 -0800

    8.128 Add ExpensesOutput components

---
+++ expense-tracker-app/components/ExpensesOutput/ExpensesList.js
@@ -0,0 +1,7 @@
+import { FlatList, Text, View } from 'react-native';
+
+const ExpensesList = () => {
+  return <FlatList />;
+};
+
+export default ExpensesList;
---
+++ expense-tracker-app/components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,12 @@
+import { FlatList, Text, View } from 'react-native';
+
+const ExpensesSummary = () => {
+  return (
+    <View>
+      <Text>Last 7 Days</Text>
+      <Text>$177.95</Text>
+    </View>
+  );
+};
+
+export default ExpensesSummary;
