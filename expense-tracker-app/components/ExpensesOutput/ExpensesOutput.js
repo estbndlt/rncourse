24630e22ede9edc89e8464556535a84626fe8025
Esteban de la Torre <edelatorre94@gmail.com>
2023-11-13 08:12:09 -0800

    8.134 Add fixes for showing more than one decimal

--- expense-tracker-app/components/ExpensesOutput/ExpensesOutput.js
+++ expense-tracker-app/components/ExpensesOutput/ExpensesOutput.js
@@ -8,13 +8,13 @@ const DUMMY_EXPENSES = [
   {
     id: 'e1',
     description: 'A pair of shoes',
-    amount: 59.99,
+    amount: 59.999,
     date: new Date('2021-12-19'),
   },
   {
     id: 'e2',
     description: 'Trousers',
-    amount: 89.29,
+    amount: 89.292,
     date: new Date('2022-01-05'),
   },
   {
