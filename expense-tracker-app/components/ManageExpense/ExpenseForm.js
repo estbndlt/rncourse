feedc2f10f47e421e8f3433912b186deccfeea5b
Esteban de la Torre <edelatorre94@gmail.com>
2023-11-16 08:34:23 -0800

    9.157 Switch from alert to message displayed within form

--- expense-tracker-app/components/ManageExpense/ExpenseForm.js
+++ expense-tracker-app/components/ManageExpense/ExpenseForm.js
@@ -10,39 +10,65 @@ const ExpenseForm = ({
   submitButtonLabel,
   defaultValues,
 }) => {
-  const [inputValues, setInputValues] = useState({
-    amount: defaultValues ? defaultValues.amount.toString() : '',
-    date: defaultValues ? getFormattedDate(defaultValues.date) : '',
-    description: defaultValues ? defaultValues.description : '',
+  const [inputs, setInputs] = useState({
+    amount: {
+      value: defaultValues ? defaultValues.amount.toString() : '',
+      isValid: true,
+    },
+    date: {
+      value: defaultValues ? getFormattedDate(defaultValues.date) : '',
+      isValid: true,
+    },
+    description: {
+      value: defaultValues ? defaultValues.description : '',
+      isValid: true,
+    },
   });
 
   const inputChangedHandler = (inputIdentifier, enteredValue) => {
-    setInputValues((curInputValues) => {
+    setInputs((curInputs) => {
       return {
-        ...curInputValues,
-        [inputIdentifier]: enteredValue,
+        ...curInputs,
+        [inputIdentifier]: { value: enteredValue, isValid: true },
       };
     });
   };
 
   const submitHandler = () => {
     const expenseData = {
-      amount: +inputValues.amount,
-      date: new Date(inputValues.date),
-      description: inputValues.description,
+      amount: +inputs.amount.value,
+      date: new Date(inputs.date.value),
+      description: inputs.description.value,
     };
 
     const amountIsValid = !isNaN() && expenseData.amount > 0;
     const dateIsValid = expenseData.date.toString() !== 'Invalid Date';
-    const descriptionIsValid = expenseData.description.trim().length > 0;
+    const descriptionIsValid =
+      expenseData.description?.trim() &&
+      expenseData.description.trim().length > 0;
 
     if (!amountIsValid || !dateIsValid || !descriptionIsValid) {
-      Alert.alert('Invalid input', 'Please check input your values');
+      // Alert.alert('Invalid input', 'Please check input your values');
+      setInputs((curInputs) => {
+        return {
+          amount: { value: curInputs.amount.value, isValid: amountIsValid },
+          date: { value: curInputs.amount.date, isValid: dateIsValid },
+          description: {
+            value: curInputs.amount.description,
+            isValid: descriptionIsValid,
+          },
+        };
+      });
       return;
     }
 
     onSubmit(expenseData);
   };
+
+  const formIsInvalid =
+    !inputs.amount.isValid ||
+    !inputs.date.isValid ||
+    !inputs.description.isValid;
 
   return (
     <View style={styles.form}>
@@ -54,7 +80,7 @@ const ExpenseForm = ({
           textInputConfig={{
             keyboardType: 'decimal-pad',
             onChangeText: inputChangedHandler.bind(this, 'amount'),
-            value: inputValues.amount,
+            value: inputs.amount.value,
           }}
         />
         <Input
@@ -64,7 +90,7 @@ const ExpenseForm = ({
             placeholder: 'YYYY-MM-DD',
             maxLength: 10,
             onChangeText: inputChangedHandler.bind(this, 'date'),
-            value: inputValues.date,
+            value: inputs.date.value,
           }}
         />
       </View>
@@ -75,9 +101,12 @@ const ExpenseForm = ({
           autoCorrect: true,
           // autoCapitalize: 'sentences', // this is default already
           onChangeText: inputChangedHandler.bind(this, 'description'),
-          value: inputValues.description,
+          value: inputs.description.value,
         }}
       />
+      {formIsInvalid && (
+        <Text>Invalid input values. Please check your entered data!</Text>
+      )}
       <View style={styles.buttons}>
         <Button style={styles.button} mode="flat" onPress={onCancel}>
           Cancel
