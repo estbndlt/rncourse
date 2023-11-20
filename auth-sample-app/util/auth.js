b64ca68105aab7dca258afbe3833950d763f40f5
Esteban de la Torre <edelatorre94@gmail.com>
2023-11-20 08:50:23 -0800

    11.176 Add createUser function using firebase backend

---
+++ auth-sample-app/util/auth.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const API_KEY = 'AIzaSyAXBhAqoBmXqUSuXTTwttxzmwCStHQC_ZE';
+
+export const createUser = async (email, password) => {
+  const rsp = await axios.post(
+    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
+    {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    }
+  );
+};
