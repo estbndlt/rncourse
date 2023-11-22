179d686cc707c208b0376b935b986e7f98517c69
Esteban de la Torre <edelatorre94@gmail.com>
2023-11-21 21:49:48 -0800

    11.184 Add auth requirement for dummy message endpoint

--- auth-sample-app/screens/WelcomeScreen.js
+++ auth-sample-app/screens/WelcomeScreen.js
@@ -1,10 +1,29 @@
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AuthContext } from '../store/auth-context';
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState('');
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://react-native-course-111bf-default-rtdb.firebaseio.com/message.json?auth=' +
+          token
+      )
+      .then((rsp) => {
+        setFetchedMessage(rsp.data);
+      });
+  }, []);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
