748d40ad3694f0600404eb538d276a7b3083ef21
Esteban de la Torre <edelatorre94@gmail.com>
2023-11-24 16:38:43 -0800

    12.198 Add permissions for ios

--- locations-app/components/Places/ImagePicker.js
+++ locations-app/components/Places/ImagePicker.js
@@ -1,8 +1,39 @@
-import { Button, View } from 'react-native';
-import { launchCameraAsync } from 'expo-image-picker';
+import { Alert, Button, View } from 'react-native';
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from 'expo-image-picker';
 
 const ImagePicker = () => {
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+
+  const verifyPermissions = async () => {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        'Insufficient Permissions',
+        'You need to grant camera permissions to use this app.'
+      );
+      return false;
+    }
+
+    return true;
+  };
+
   const takeImageHandler = async () => {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
     const image = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
