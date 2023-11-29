777c9b4eb7a4db71803f68d3eec9e1dd78e0c97e
Esteban de la Torre <edelatorre94@gmail.com>
2023-11-29 08:19:44 -0800

    12.217 Add ability to fetch stored places from database

--- locations-app/screens/AllPlaces.js
+++ locations-app/screens/AllPlaces.js
@@ -1,14 +1,21 @@
 import { useEffect, useState } from 'react';
 import PlacesList from '../components/Places/PlacesList';
 import { useIsFocused } from '@react-navigation/native';
+import { fetchPlaces } from '../util/database';
 
 const AllPlaces = ({ route }) => {
   const [loadedPlaces, setLoadedPlaces] = useState([]);
   const isFocused = useIsFocused();
 
   useEffect(() => {
-    if (isFocused && route.params) {
-      setLoadedPlaces((curPlaces) => [...curPlaces, route.params.place]);
+    const loadPlaces = async () => {
+      const places = await fetchPlaces();
+      setLoadedPlaces(places);
+    };
+
+    if (isFocused) {
+      loadPlaces();
+      // setLoadedPlaces((curPlaces) => [...curPlaces, route.params.place]);
     }
   }, [isFocused, route]);
 
